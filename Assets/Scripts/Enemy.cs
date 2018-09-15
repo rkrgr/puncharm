f701690c8486e31243e746b58781ae6b0061b323
NeonNP <rene.krueger94@gmail.com>
2018-09-15 17:24:45 +0200

    add enemy that follows player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    const int moveLeft = -1;
+    const int moveRight = 1;
+
+    public float speed;
+
+    public Rigidbody2D player;
+
+    Rigidbody2D rb;
+
+    int moveDirection = moveLeft;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if(rb.position.x > player.position.x)
+        {
+            moveDirection = moveLeft;
+        }
+        else
+        {
+            moveDirection = moveRight;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection * speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+}

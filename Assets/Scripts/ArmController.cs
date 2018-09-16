ea01f351d5f509c52c7e41450b0866726e2e9168
NeonNP <rene.krueger94@gmail.com>
2018-09-16 20:38:04 +0200

    add punching

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class ArmController : MonoBehaviour {
 
-    public float offset;
+    public Vector2 positionOffset;
+    public float rotationOffset;
 
     public Transform player;
     public GameObject arm;
+    public GameObject fist;
+
+    public float punchSpeed;
+    public float punchDistance;
+
+    bool isPunching = false;
+    float fistDistance;
+    Vector3 initialFistPosition;
+    bool fistExpanding;
 	
 	void Update () {
 
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        if(!isPunching)
+        {
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                initialFistPosition = fist.transform.localPosition;
+                isPunching = true;
+                fistExpanding = true;
+            }
+        }
+        else
+        {
+            fistDistance = Vector3.Distance(initialFistPosition, fist.transform.localPosition);
+            if(fistExpanding && fistDistance >= punchDistance)
+            {
+                fistExpanding = false;
+            }
+            else if(!fistExpanding && fistDistance <= 1)
+            {
+                isPunching = false;
+                fist.transform.localPosition = initialFistPosition;
+            }
+        }
 	}
+
+    void FixedUpdate()
+    {
+        if (isPunching)
+        {
+            if(fistExpanding)
+            {
+                fist.transform.Translate(Vector2.down * punchSpeed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                fist.transform.Translate(-Vector2.down * punchSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.position + new Vector3(positionOffset.x, positionOffset.y);
+    }
 }

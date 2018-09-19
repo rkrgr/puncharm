10c554679ec7b91a38d099295ff45f965112b6eb
NeonNP <rene.krueger94@gmail.com>
2018-09-19 17:38:05 +0200

    refactor arm

--- Assets/Scripts/Arm.cs
+++ Assets/Scripts/Arm.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArmController : MonoBehaviour {
+public class Arm : MonoBehaviour {
 
     public bool mouseControle = true;
 
@@ -11,17 +11,25 @@ public class ArmController : MonoBehaviour {
 
     public Transform player;
 	
-	void Update () {
-
-        Vector2 armDirection = mouseControle ? Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position
-                                                : new Vector3(Input.GetAxisRaw("RightJoystickHorizontal"), -Input.GetAxisRaw("RightJoystickVertical"));
+	void Update ()
+    {
+        Vector2 armDirection = mouseControle ? MoveArmMouse() : MoveArmJoystick();
         float rotZ = Mathf.Atan2(armDirection.y, armDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
-
     }
 
     void LateUpdate()
     {
         transform.position = player.position + new Vector3(positionOffset.x, positionOffset.y);
+    }
+
+    private Vector3 MoveArmJoystick()
+    {
+        return new Vector3(Input.GetAxisRaw("RightJoystickHorizontal"), -Input.GetAxisRaw("RightJoystickVertical"));
+    }
+
+    private Vector3 MoveArmMouse()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
 }

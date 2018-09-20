7a168d2125ed735f827c4fe13ed0720c1f32e15d
NeonNP <rene.krueger94@gmail.com>
2018-09-20 22:00:59 +0200

    redone punching

--- Assets/Scripts/Arm.cs
+++ Assets/Scripts/Arm.cs
@@ -4,67 +4,38 @@ using UnityEngine;
 
 public class Arm : MonoBehaviour {
 
-    public bool mouseControle = true;
-
     public Vector2 positionOffset;
     public float rotationOffset;
 
     public Transform player;
 
-    public string[] collideWith;
-    [Range(0, 10)]
-    public float collideDistanceOffset;
+    public Transform aimMark;
 
-    Fist fist;
-
-    int collideLayerMask;
+    Punch punch;
 
     void Awake()
     {
-        fist = gameObject.GetComponentInChildren<Fist>();
+        punch = gameObject.GetComponent<Punch>();
     }
 
     void Start()
     {
-        collideLayerMask = LayerMask.GetMask(collideWith);
+        Physics2D.IgnoreLayerCollision(8, 8);
     }
 
 	void Update ()
     {
-        Vector2 armDirection = mouseControle ? MoveArmMouse() : MoveArmJoystick();
-
-        float dis = fist.fistDistance - collideDistanceOffset;
-        Debug.Log(dis);
-        if (dis < 0) dis = 0;
-        RaycastHit2D hit = CheckRaycast(armDirection, dis);
-
-        bool canMove = !fist.isPunching || !hit.collider;
-
-        if(canMove)
+        Vector2 armDirection;
+        if (punch.isPunching)
         {
-            Quaternion quatOld = transform.rotation;
-            float rotZ = Mathf.Atan2(armDirection.y, armDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+            armDirection = aimMark.position - transform.position;
         }
-    }
-
-    void LateUpdate()
-    {
+        else
+        {
+            armDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        }
+        float rotZ = Mathf.Atan2(armDirection.y, armDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
         transform.position = player.position + new Vector3(positionOffset.x, positionOffset.y);
-    }
-
-    private Vector3 MoveArmJoystick()
-    {
-        return new Vector3(Input.GetAxisRaw("RightJoystickHorizontal"), -Input.GetAxisRaw("RightJoystickVertical"));
-    }
-
-    private Vector3 MoveArmMouse()
-    {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-    }
-
-    private RaycastHit2D CheckRaycast(Vector2 direction, float currentDistance)
-    {
-        return Physics2D.Raycast(transform.position, direction, currentDistance, collideLayerMask);
     }
 }
---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour {
+
+    public GameObject impactEffect;
+
+    public float punchSpeed;
+    public float punchLoadSpeed;
+
+    public float minPunchDistance;
+    public float maxPunchDistance;
+
+    private float currentAimDistance;
+
+    public int damage;
+
+    public Transform aimMark;
+    public Transform fist;
+
+    internal bool isPunching = false;
+    bool isFistExpanding = false;
+    bool isLoading = false;
+    Vector3 initialLocalFistPosition;
+    Vector2 aimMarkPos;
+
+    void Start()
+    {
+        initialLocalFistPosition = fist.localPosition;
+    }
+
+    void Update()
+    {
+        if (!isPunching && Input.GetMouseButtonDown(0))
+        {
+            isLoading = true;
+        }
+
+        if (!isPunching && Input.GetMouseButtonUp(0))
+        {
+            aimMarkPos = aimMark.position;
+            isPunching = true;
+            isFistExpanding = true;
+            isLoading = false;
+        }
+
+        if (isPunching)
+        {
+            aimMark.position = aimMarkPos;
+        }
+        
+        if(!isPunching && !isLoading)
+        {
+
+            aimMark.localPosition = initialLocalFistPosition;
+            aimMark.Translate(Vector3.down * minPunchDistance);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isLoading)
+        {
+            aimMark.Translate(Vector3.down * punchLoadSpeed * Time.fixedDeltaTime);
+        }
+
+        if (isPunching)
+        {
+            if (isFistExpanding)
+            {
+                fist.position = Vector2.MoveTowards(fist.position, aimMark.position, punchSpeed * Time.fixedDeltaTime);
+
+                if(fist.position == aimMark.position)
+                {
+                    isFistExpanding = false;
+                }
+            }
+            else
+            {
+                fist.localPosition = Vector2.MoveTowards(fist.localPosition, initialLocalFistPosition, punchSpeed * Time.fixedDeltaTime);
+
+                if (fist.localPosition == initialLocalFistPosition)
+                {
+                    isPunching = false;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if(hitInfo.gameObject.layer == 9)
+        {
+            isFistExpanding = false;
+        }
+
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null && isFistExpanding)
+        {
+            enemy.TakeDamage(damage);
+            Instantiate(impactEffect, transform.position, transform.rotation);
+            isFistExpanding = false;
+        }
+    }
+}

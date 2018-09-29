5739e6f82e204ab55164ea7c14789b8be533b4e2
NeonNP <rene.krueger94@gmail.com>
2018-09-29 18:23:42 +0200

    add UI health display

---
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour {
+
+    PlayerHealth health;
+    Text uiText;
+
+    void Awake()
+    {
+        health = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+        uiText = GetComponent<Text>();
+    }
+	
+	void Update () {
+        uiText.text = "Health: " + health.GetHealth() + " / " + health.maxHealth;
+	}
+}

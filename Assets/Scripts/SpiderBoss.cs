ad50b5b486c7b102ec83f50cf97b72ba0208f427
NeonNP <rene.krueger94@gmail.com>
2018-10-04 17:21:06 +0200

    modify spider boss so that it jumps when in line with player

--- Assets/Scripts/SpiderBoss.cs
+++ Assets/Scripts/SpiderBoss.cs
@@ -11,30 +11,61 @@ public class SpiderBoss : MonoBehaviour {
     Vector2 moveDirection;
     Vector2 gravityDirection = Vector2.down;
     float colliderHalfWidth;
+    float colliderHalfHeight;
+
+    bool jump = false;
 
     Rigidbody2D rb;
+
+    GameObject player;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Start()
     {
         BoxCollider2D col = GetComponent<BoxCollider2D>();
         colliderHalfWidth = col.size.x * transform.lossyScale.x / 2;
+        colliderHalfHeight = col.size.y * transform.lossyScale.y / 2;
         SetMoveRight();
+    }
+
+    void Update()
+    {
+        if (!jump && (transform.position.y < player.transform.position.y + .5f && transform.position.y > player.transform.position.y - .1f
+            || transform.position.x < player.transform.position.x + .5f && transform.position.x > player.transform.position.x - .5f))
+        {
+            jump = true;
+            transform.Rotate(Vector3.forward, 180f);
+        }
+        Debug.Log(jump);
     }
 
 	void FixedUpdate () {
 
-        if (CollisonRight())
+        if(!jump)
         {
-            transform.Rotate(Vector3.forward, 90f);
-            gravityDirection = -transform.up;
-        }
+            if (CollisonRight())
+            {
+                transform.Rotate(Vector3.forward, 90f);
+                gravityDirection = -transform.up;
+            }
 
-        Move();
+            Move();
+        }
+        else // jump
+        {
+            gravityDirection = transform.up;
+            rb.velocity = -transform.up * speed * 2 * Time.deltaTime;
+
+            if (CollisonDown())
+            {
+                jump = false;
+            }
+        }
 	}
 
     void Move()
@@ -50,6 +81,11 @@ public class SpiderBoss : MonoBehaviour {
     bool CollisonRight()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position + transform.right * colliderHalfWidth, transform.right, 0.05f, obstacle);
+        return hit.collider != null;
+    }
+    bool CollisonDown()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - transform.up * colliderHalfHeight, -transform.up, 0.5f, obstacle);
         return hit.collider != null;
     }
 }

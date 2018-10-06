0a1b3c42c3a9f5ade4719e4bda7f38ae4482f85b
JPTorkx <waehner.jp@gmail.com>
2018-10-06 16:22:36 +0200

    demoscene created. tests and testresources put in Editor folder.

---
+++ Assets/Editor/Tests/PlayModeTests/TestPlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestPlayerDeath {
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestPlayerDeathWithEnumeratorPasses() {
+        
+        yield return null;
+    }
+}
---
+++ Assets/Editor/Tests/PlayModeTests/TestplaygroundSetup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TestplaygroundSetup : IPrebuildSetup
+{
+    public Tilemap Tilemap { get; private set; }
+
+    private Tile tile;
+
+    public void Setup()
+    {
+
+    }
+}

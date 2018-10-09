fb45572d4a689236fffa68c9ebb388fb601e3e4e
Sackopeppler <rene.krueger94@gmail.com>
2018-10-09 20:28:16 +0200

    add GrenadeRobot that is throwing grenades which do nothing

---
+++ Assets/Scripts/Enemy/GrenadeRobot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeRobot : Enemy {
+
+    public Rigidbody2D grenade;
+    public float grenadeSpeed = 1f;
+
+    public float attackCooldown = 1f;
+    float currentAttackCooldown = 0f;
+	
+    void Update () {
+        if(currentAttackCooldown <= 0f)
+        {
+            currentAttackCooldown = attackCooldown;
+            Rigidbody2D grenadeClone = (Rigidbody2D)Instantiate(grenade, transform.position, transform.rotation);
+            grenadeClone.velocity = (Vector2.left + Vector2.up * 5).normalized * grenadeSpeed;
+        }
+        
+        if(currentAttackCooldown > 0f)
+        {
+            currentAttackCooldown -= Time.deltaTime;
+        }
+    }
+}

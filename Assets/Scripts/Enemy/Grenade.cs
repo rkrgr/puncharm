30108853140548a2b4478616bfbd472bac1241e8
Sackopeppler <rene.krueger94@gmail.com>
2018-10-10 23:24:30 +0200

    modify grenade so that it does damage to player

---
+++ Assets/Scripts/Enemy/Grenade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Grenade : MonoBehaviour {
+
+    public int damage;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerDamage playerDamage = other.GetComponent<PlayerDamage>();
+        if(playerDamage != null)
+        {
+            playerDamage.Hit(damage);
+        }
+
+        Destroy(gameObject);
+    }
+
+}

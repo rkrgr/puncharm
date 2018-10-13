6f6a086e9e1c7f97796bce10681305f5d6655238
Sackopeppler <rene.krueger94@gmail.com>
2018-10-13 16:49:45 +0200

    modify GrenadeRobot so that it is facing player

--- Assets/Scripts/Enemy/GrenadeRobot.cs
+++ Assets/Scripts/Enemy/GrenadeRobot.cs
@@ -9,18 +9,47 @@ public class GrenadeRobot : Enemy {
 
     public float attackCooldown = 1f;
     float currentAttackCooldown = 0f;
-	
+
+    public Transform player;
+
+    Vector2 faceDirection = Vector2.right;
+
     void Update () {
+        if(player.position.x < transform.position.x && faceDirection == Vector2.right)
+        {
+            FlipCharacter();
+        }
+        else if(player.position.x > transform.position.x && faceDirection == Vector2.left)
+        {
+            FlipCharacter();
+        }
+
         if(currentAttackCooldown <= 0f)
         {
             currentAttackCooldown = attackCooldown;
             Rigidbody2D grenadeClone = (Rigidbody2D)Instantiate(grenade, transform.position, transform.rotation);
-            grenadeClone.velocity = (Vector2.left + Vector2.up * 5).normalized * grenadeSpeed;
+            grenadeClone.velocity = (faceDirection + Vector2.up * 5).normalized * grenadeSpeed;
         }
         
         if(currentAttackCooldown > 0f)
         {
             currentAttackCooldown -= Time.deltaTime;
+        }
+    }
+
+    void FlipCharacter()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
+
+        if (faceDirection == Vector2.right)
+        {
+            faceDirection = Vector2.left;
+        }
+        else
+        {
+            faceDirection = Vector2.right;
         }
     }
 }
